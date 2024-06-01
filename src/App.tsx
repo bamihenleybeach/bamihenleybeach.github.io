6cf9275c68bb55153d46d29f1f9b0b0b040ad2f4
LinhLV <levulinh291991@gmail.com>
2024-06-02 08:10:38 +0930

    Update UI

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,37 @@ function App() {
       <div className="row">
         <div className="column">
           <h1 className="title">DONE</h1>
+          <ul>
+            <li>
+              <div>
+                <h2>Mark</h2>
+                <button>Delivered</button>
+              </div>
+            </li>
+            <li>
+              <div>
+                <h2>Mark</h2>
+                <button>Delivered</button>
+              </div>
+            </li>
+          </ul>
         </div>
         <div className="column">
           <h1 className="title">DOING</h1>
+          <ul>
+            <li>
+              <div>
+                <h2>Mark</h2>
+                <button>Done</button>
+              </div>
+            </li>
+            <li>
+              <div>
+                <h2>Mark</h2>
+                <button>Done</button>
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
