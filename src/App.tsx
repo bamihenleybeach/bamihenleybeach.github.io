298e945ea0fe00c00fda0f34a5d7c7b268cea856
LinhLV <levulinh291991@gmail.com>
2024-06-02 08:05:27 +0930

    Update UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React2
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="column">
+          <h1 className="title">DONE</h1>
+        </div>
+        <div className="column">
+          <h1 className="title">DOING</h1>
+        </div>
+      </div>
     </div>
   );
 }
