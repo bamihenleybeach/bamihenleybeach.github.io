6e2b5f05783ec574de2d31a0be6eafc258c663b8
LinhLV <levulinh291991@gmail.com>
2024-06-02 09:11:35 +0930

    Update UI

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,10 @@ import { AppContext } from './AppContext';
 import { writeNewOrder, writeVersion } from './utils/firebaseUtils';
 
 function App() {
+  const params = new URLSearchParams(window.location.hash.replace('#', ''));
+  const mode = params.get('mode');
   const {state} = useContext(AppContext);
-
+  console.log(mode)
   const onClickDone = () => {
     writeVersion();
   }
@@ -24,13 +26,17 @@ function App() {
             <li>
               <div className="record">
                 <h2>Mark</h2>
-                <button>Delivered</button>
+                {
+                  mode == 'admin' ? <button>Delivered</button> : null
+                }
               </div>
             </li>
             <li>
               <div className="record">
                 <h2>Mark</h2>
-                <button>Delivered</button>
+                {
+                  mode == 'admin' ? <button>Delivered</button> : null
+                }
               </div>
             </li>
           </ul>
@@ -41,29 +47,45 @@ function App() {
             <li>
               <div className="record">
                 <h2>Mark</h2>
-                <button>Done</button>
+                {
+                  mode == 'admin' ? <button>Done</button> : null
+                }
               </div>
             </li>
             <li>
               <div className="record">
                 <h2>Mark</h2>
-                <button onClick={onClickDone}>Done</button>
+                {
+                  mode == 'admin' ? <button onClick={onClickDone}>Done</button> : null
+                }
               </div>
             </li>
           </ul>
         <i>{state.version}</i>
         </div>
       </div>
+<<<<<<< HEAD
       <form onSubmit={onSubmitNewOrder}>
+=======
+      {
+        mode == 'admin' ? 
+>>>>>>> 7e9acc8 (Update UI)
         <div className="input-group">
           <div className="customer-name-input">
             <input id="customer-name" type="text" placeholder="Customer Name"/>
           </div>
           <div className="customer-name-button">
+<<<<<<< HEAD
             <button id="add-order" type="submit">Add</button>
           </div>
         </div>
       </form>
+=======
+            <button id="add-order" type="submit">Doing</button>
+          </div>
+        </div> : null
+      }
+>>>>>>> 7e9acc8 (Update UI)
     </div>
   );
 }
